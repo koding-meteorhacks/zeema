91bee12fbca8ccde3447d066bf7244d657675649
Chathu Vishwajith <4evercreations@gmail.com>
2014-12-07 21:02:18 +0530

    Fixed Image URL Issue

--- public/api.js
+++ public/api.js
@@ -1,11 +1,7 @@
-var css = document.createElement("style");
-css.type = "text/css";
-css.innerHTML =" #zeema-no-agree-bar { position: fixed; bottom: 0; left: 0; right: 0; padding: 20px 20px; border-top: 6px solid #F85E5E; background-color: #fff; } #zeema-no-agree-bar { color: rgb(51, 51, 51); display: block; font-family: 'Arial', sans-serif; font-size: 16px; line-height: 26px; } #zeema-revice-now { margin-left: 10px; font-size: 14px; line-height: 24px; font-weight: bold; padding: 5px 10px 5px 10px; background-color: #FFE1D2; border: 1px solid #E0AAAA; text-decoration: none; color: #222; cursor: pointer; border-radius: 3px; } #zeema-revice-now:hover { opacity: 0.8; } #zeema-hide { position: absolute; right: 20px; top: 25px; border: 0; background-color: #fff; padding: 0px; color: #111; font-size: 16px; cursor: pointer; } #zeema-hide:hover { opacity: 0.8;} #zeema-badge { position:fixed; bottom:10px; left:10px; width:50px; height:50px; z-index:10; cursor: pointer;} .zeema-error-badge {  background: url('notverified.svg'); background-size: 50px 50px; } .zeema-ok-badge {  background: url('verified.svg'); background-size: 50px 50px; } ";
-document.body.appendChild(css);
 Zeema = {};
 Zeema.check = function(appId,email,url){
 	url = typeof url !== 'undefined' ? url : "https://zeema.ngrok.com";
-	
+
 	Zeema.url = url;
 	Zeema.appId = appId;
 	Zeema.email = email;
@@ -50,6 +46,11 @@ Zeema._showHideButton = function(){
 	zeema_hide.innerHTML+= "x";
 	zeema_div.appendChild(zeema_hide);
 }
+
+var css = document.createElement("style");
+css.type = "text/css";
+css.innerHTML =" #zeema-no-agree-bar { position: fixed; bottom: 0; left: 0; right: 0; padding: 20px 20px; border-top: 6px solid #F85E5E; background-color: #fff; } #zeema-no-agree-bar { color: rgb(51, 51, 51); display: block; font-family: 'Arial', sans-serif; font-size: 16px; line-height: 26px; } #zeema-revice-now { margin-left: 10px; font-size: 14px; line-height: 24px; font-weight: bold; padding: 5px 10px 5px 10px; background-color: #FFE1D2; border: 1px solid #E0AAAA; text-decoration: none; color: #222; cursor: pointer; border-radius: 3px; } #zeema-revice-now:hover { opacity: 0.8; } #zeema-hide { position: absolute; right: 20px; top: 25px; border: 0; background-color: #fff; padding: 0px; color: #111; font-size: 16px; cursor: pointer; } #zeema-hide:hover { opacity: 0.8;} #zeema-badge { position:fixed; bottom:10px; left:10px; width:50px; height:50px; z-index:10; cursor: pointer;} .zeema-error-badge {  background: url('"+Zeema.url+"notverified.svg'); background-size: 50px 50px; } .zeema-ok-badge {  background: url('"+Zeema.url+"verified.svg'); background-size: 50px 50px; } ";
+document.body.appendChild(css);
 
 window._zeema_check = function(payload){
 	var isHide =readCookie('zeema');
